f7ffdb41266a4f901d7d2b9b71d9f2d835eb4c21
mehdi <kamani@ipreez.ir>
2022-10-30 16:31:00 +0330

    add tests for owns

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,6 +1,8 @@
 import pytest
 from unittest import mock
+from datetime import datetime
 
+import schemas
 from adapters.repository import TodoRepository
 from services.permissions import DeveloperPermService
 
@@ -37,3 +39,45 @@ async def test_memberof_returns_false_when_user_not_in_project(fake_repo, create
 
     assert result is False
     mock_user_in_project.assert_called_once_with(fake_repo, user_id=user_id, project_id=project_id)
+
+
+@pytest.mark.asyncio
+async def test_owns_returns_true(fake_repo, create_mock_coro):
+    mock_get_project_by_id, _ = create_mock_coro("adapters.repository.TodoRepository.get_project_by_id")
+    mock_get_project_by_id.return_value = schemas.ProjectInDb(owner=100, title="test project", id=1, created_at=datetime.now())
+    perm_service = DeveloperPermService(fake_repo)
+    user_id = 100
+    project_id = 200
+
+    result = await perm_service._owns(user_id=user_id, project_id=project_id)
+
+    assert result is True
+    mock_get_project_by_id.assert_called_once_with(fake_repo, project_id)
+
+
+@pytest.mark.asyncio
+async def test_owns_returns_true(fake_repo, create_mock_coro):
+    mock_get_project_by_id, _ = create_mock_coro("adapters.repository.TodoRepository.get_project_by_id")
+    mock_get_project_by_id.return_value = schemas.ProjectInDb(owner=100, title="test project", id=1, created_at=datetime.now())
+    perm_service = DeveloperPermService(fake_repo)
+    user_id = 100
+    project_id = 200
+
+    result = await perm_service._owns(user_id=user_id, project_id=project_id)
+
+    assert result is True
+    mock_get_project_by_id.assert_called_once_with(fake_repo, project_id)
+
+
+@pytest.mark.asyncio
+async def test_owns_returns_false(fake_repo, create_mock_coro):
+    mock_get_project_by_id, _ = create_mock_coro("adapters.repository.TodoRepository.get_project_by_id")
+    mock_get_project_by_id.return_value = schemas.ProjectInDb(owner=101, title="test project", id=1, created_at=datetime.now())
+    perm_service = DeveloperPermService(fake_repo)
+    user_id = 100
+    project_id = 200
+
+    result = await perm_service._owns(user_id=user_id, project_id=project_id)
+
+    assert result is False
+    mock_get_project_by_id.assert_called_once_with(fake_repo, project_id)
